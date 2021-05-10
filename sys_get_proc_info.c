b7ad18c2a7f55065794973af8701359374621ad4
MinhHuy <huy.cse.9@gmail.com>
2021-05-10 10:08:04 +0000

    Rewrite file

--- sys_get_proc_info.c
+++ sys_get_proc_info.c
@@ -3,51 +3,66 @@
 #include <linux/sched/signal.h>
 #include <linux/errno.h>
 #include <linux/string.h>
-#include <linux/unistd.h>
 #include <asm/current.h>
-#include <asm/uaccess.h>
-#include <linux/pid.h>
-typedef struct proc_info {
+#include <linux/linkage.h>
+#include <linux/uaccess.h>
+
+typedef struct {
 	pid_t pid;
 	char name[16];
 } proc_info;
 
-typedef struct procinfos {
+typedef struct {
 	long studentID;
 	proc_info proc;
 	proc_info parent_proc;
 	proc_info oldest_child_proc;
 } procinfos;
 
-SYSCALL_DEFINE0(get_proc_info, pid_t, pid, procinfos*, info) {
-	struct task_struct* task;
-	struct task_struct* oldest_child;
-	struct list_head* list;
-
+struct task_struct* task;
+struct task_struct* oldest_child;
+struct list_head* list;
+procinfos temp;
+int result;
+pid_t kernelpid;
+asmlinkage long sys_get_proc_info(pid_t pid, procinfos* info) {
 	// To do
-	printk("%d\n", pid);
-	if(pid == -1) task = current;
-	else task = pid_task(find_vpid(pid), PIDTYPE_PID);
-	if(task == NULL) {
-		printk("Cannot find process\n");
-		return EINVAL;
+	result = copy_from_user(&kernelpid, &pid, sizeof(pid_t));
+	if (result == 0) {
+		printk("Copy kernelpid successfully\n");
 	}
-	info->studentID = 1915940;
+	else {
+		printk("Copy kernel pid fail\n");
+	}	
+	if(kernelpid == -1) 
+		task = current;
+	else 
+		task = pid_task(find_vpid(kernelpid), PIDTYPE_PID);
+	if(task == NULL) 
+		return EINVAL;
+
+	temp.studentID = 1915940;
 
 	printk("[%d]--------------[%s]\n", task->pid, task->comm);
-	info->proc.pid = task->pid;
-	strcpy(info->proc.name, task->comm);
+	temp.proc.pid = task->pid;
+	strcpy(temp.proc.name, task->comm);
 
 	printk("[%d]--------------[%s]\n", task->parent->pid, task->parent->comm);
-	info->parent_proc.pid = task->parent->pid;
-	strcpy(info->parent_proc.name, task->parent->comm);
+	temp.parent_proc.pid = task->parent->pid;
+	strcpy(temp.parent_proc.name, task->parent->comm);
 
 	list = &task->children;
 	oldest_child = list_entry(list->next, struct task_struct, sibling);
-
 	printk("[%d]--------------[%s]\n", oldest_child->pid, oldest_child->comm);
-	info->oldest_child_proc.pid = oldest_child->pid;
-	strcpy(info->oldest_child_proc.name, oldest_child->comm);
+	temp.oldest_child_proc.pid = oldest_child->pid;
+	strcpy(temp.oldest_child_proc.name, oldest_child->comm);
 
+	result = copy_to_user(info, &temp, sizeof(procinfos));
+	if (result == 0) {
+		printk("Copy data successfully\n");
+	}
+	else {
+		printk("Copy fail\n");
+	}
 	return 0;
 }
