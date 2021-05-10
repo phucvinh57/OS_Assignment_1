2d71713f7b0163883115ef9511b9004caa8663ae
Nguyen Phuc Vinh <nguyenphucvinh05072001@gmail.com>
2021-05-10 23:54:44 +0700

    kernel code

--- sys_get_proc_info.c
+++ sys_get_proc_info.c
@@ -19,50 +19,54 @@ struct procinfos {
 	struct proc_info oldest_child_proc;
 };
 
-
-struct task_struct* task;
-struct task_struct* oldest_child;
-struct list_head* list;
-struct procinfos temp;
-pid_t kernelpid;
-asmlinkage long sys_get_proc_info(pid_t pid, struct procinfos* info) {
+SYSCALL_DEFINE2(get_proc_info, pid_t, pid, struct procinfos* info) {
+//asmlinkage long sys_get_proc_info(pid_t pid, struct procinfos* info) {
 	// To do
-	if(__get_user(kernelpid, &pid) == 0)
-		printk("Copy kernelpid successfully\n");
-	else {
-		printk("Copy kernel pid fail\n");
-		return -EFAULT;
-	}
+	struct task_struct* task;
+	struct task_struct* oldest_child;
+	struct list_head* list;
+	struct procinfos temp;
 
-	printk("Param value: %d\n", kernelpid);
-	if(kernelpid == -1) task = current;
-	else task = pid_task(find_vpid(kernelpid), PIDTYPE_PID);
-	if(task == NULL) {
-		printk("Cannot find process\n");
-		return EINVAL;
-	}
+	if(pid < -1 || info == NULL) return EINVAL;
+	if(pid == -1) task = current;
+	if(pid == 0) task = pid_task(find_vpid(1), PIDTYPE_PID)->parent;
+	if(pid >= 1) task = pid_task(find_vpid(pid), PIDTYPE_PID);
+	if(task == NULL) return EINVAL;
+
 	temp.studentID = 1915940;
-
 	printk("[%d]--------------[%s]\n", task->pid, task->comm);
 	temp.proc.pid = task->pid;
 	strcpy(temp.proc.name, task->comm);
 
-	printk("[%d]--------------[%s]\n", task->parent->pid, task->parent->comm);
-	temp.parent_proc.pid = task->parent->pid;
-	strcpy(temp.parent_proc.name, task->parent->comm);
+	if(task == task->parent) {
+		printk("This process has no parent\n");
+		temp.parent_proc.pid = -1;
+		strcpy(temp.parent_proc.name, "null");
+	} else {
+		printk("[%d]--------------[%s]\n", task->parent->pid, task->parent->comm);
+		temp.parent_proc.pid = task->parent->pid;
+		strcpy(temp.parent_proc.name, task->parent->comm);
+	}
 
 	list = &task->children;
-	oldest_child = list_entry(list->next, struct task_struct, sibling);
-	printk("[%d]--------------[%s]\n", oldest_child->pid, oldest_child->comm);
-	temp.oldest_child_proc.pid = oldest_child->pid;
-	strcpy(temp.oldest_child_proc.name, oldest_child->comm);
+	if(list_empty_careful(list)) {
+		printk("This process has no child\n");
+		temp.oldest_child_proc.pid = -1;
+		strcpy(temp.oldest_child_proc.name, "NULL"); 
+	}
+	else {
+		oldest_child = list_entry(list->next, struct task_struct, sibling);
+		printk("[%d]--------------[%s]\n", oldest_child->pid, oldest_child->comm);
+		temp.oldest_child_proc.pid = oldest_child->pid;
+		strcpy(temp.oldest_child_proc.name, oldest_child->comm);
+	}
 
 	if (copy_to_user(info, &temp, sizeof(struct procinfos)) == 0) {
 		printk("Copy data successfully\n");
 	}
 	else {
 		printk("Copy fail\n");
-		return -EFAULT;
+		return EINVAL;
 	}
 	return 0;
 }
